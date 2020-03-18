468fbb90d05c77aff172b35fcb48aa4b338b2d5a
sriramrudraraju <sriram.rudraraju6@gmail.com>
2020-03-18 16:01:58 -0500

    feat: parser using regex

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
+
+import { Parser } from './parser/parser.component';
+
 import './app.css';
+
+
+const example1 = `
+<style>
+.button-basics-example {
+  color: red
+}
+</style>
+<button class="button-basics-example">Save</button>`;
+
+const example2 = `
+<main class="main">
+      <div class="quote">{quote}</div>
+      <div class="author">{author}</div>
+    </main>
+`;
 
 function App() {
   return (
     <div className="App">
-      React
+      {/* <div>
+        Simple Example :  
+        <Parser input={example1} />
+      </div> */}
+      <div>
+        Example 2
+        <Parser input={example2} interval/>
+      </div>
+      <div>
+        Example 3
+        <Parser input={example2} interval/>
+      </div>
     </div>
   );
 }
