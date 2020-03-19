f5081944fdc12f3d643a308011ae54c9870470cc
sriramrudraraju <sriram.rudraraju6@gmail.com>
2020-03-19 14:31:24 -0500

    Updates

---
+++ precache-manifest.173d15e4ba66d0ba3a983bc94adb88f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c6666650920cffea4158ebae680b5ad",
+    "url": "/html-editor/index.html"
+  },
+  {
+    "revision": "6f51b70e58631caee193",
+    "url": "/html-editor/static/css/main.3cbfcd80.chunk.css"
+  },
+  {
+    "revision": "2cd25719fff256cd94e1",
+    "url": "/html-editor/static/js/2.1055d3d2.chunk.js"
+  },
+  {
+    "revision": "3e7eb0c51ce474724fe2295510ac9693",
+    "url": "/html-editor/static/js/2.1055d3d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f51b70e58631caee193",
+    "url": "/html-editor/static/js/main.497fcf34.chunk.js"
+  },
+  {
+    "revision": "abd1b65c081b101d1572",
+    "url": "/html-editor/static/js/runtime-main.a50da933.js"
+  }
+]);
